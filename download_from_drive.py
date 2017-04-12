66195937d53ff8eb981b3205e25469c52ec75e79
Danila Tsvetkov <danilatsvet@gmail.com>
2017-04-13 01:11:08 +0300

    Fix issues on Windows 10

--- download_from_drive.py
+++ download_from_drive.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 
-import httplib2
-import os
-import time
-import datetime
-import mimetypes
-import shutil
-import io
+"""Initial comment."""
 
+import datetime
+import io
+# import mimetypes
+import os
+import shutil
+import time
+import hashlib
+import httplib2
+
+# from os import path
 from apiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
-from apiclient.http import MediaFileUpload, MediaIoBaseDownload
+from apiclient.http import MediaIoBaseDownload
 
 import initial_upload
 
@@ -22,32 +26,42 @@ SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly',
           'https://www.googleapis.com/auth/drive.file',
           'https://www.googleapis.com/auth/drive']
 CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'Drive API Python Quickstart'
+APPLICATION_NAME = 'Drive Sync application'
 
 
 # Sample (reference) map of Google Docs MIME types to possible exports
 # (for more information check about().get() method with exportFormats field)
 GOOGLE_MIME_TYPES = {
-    # 'application/vnd.google-apps.document': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-    'application/vnd.google-apps.document': 'application/vnd.oasis.opendocument.text',
-    # 'application/vnd.google-apps.spreadsheet': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-    'application/vnd.google-apps.spreadsheet': 'application/x-vnd.oasis.opendocument.spreadsheet',
-    'application/vnd.google-apps.presentation': 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
-    'application/vnd.google-apps.drawing': 'image/jpeg',
-    'application/vnd.google-apps.script': 'application/vnd.google-apps.script+json'
-    # 'application/vnd.google-apps.folder': '',
-    # 'application/vnd.google-apps.form': 'application/pdf',
-    # 'application/vnd.google-apps.fusiontable': '',
-    # 'application/vnd.google-apps.map': 'application/pdf',
-    # 'application/vnd.google-apps.photo': 'image/jpeg',
-    # 'application/vnd.google-apps.file': '',
-    # 'application/vnd.google-apps.sites': '',
-    # 'application/vnd.google-apps.unknown': '',
-    # 'application/vnd.google-apps.video': '',
-    # 'application/vnd.google-apps.audio': '',
-    # 'application/vnd.google-apps.drive-sdk': ''
-    # 'application/octet-stream': 'text/plain'
+    'application/vnd.google-apps.document':
+    ['application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+     '.docx'],
+    # 'application/vnd.google-apps.document':
+    # 'application/vnd.oasis.opendocument.text',
+    'application/vnd.google-apps.spreadsheet':
+    ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+     '.xlsx'],
+    # 'application/vnd.oasis.opendocument.spreadsheet',
+    'application/vnd.google-apps.presentation':
+    ['application/vnd.openxmlformats-officedocument.presentationml.presentation',
+     '.pptx']
+    # 'application/vnd.oasis.opendocument.presentation'
 }
+
+#Other google mimetypes in case if you need it 
+# 'application/vnd.google-apps.drawing': 'application/x-msmetafile'
+# 'application/vnd.google-apps.script': 'application/vnd.google-apps.script+json'
+# 'application/vnd.google-apps.folder': '',
+# 'application/vnd.google-apps.form': 'application/pdf',
+# 'application/vnd.google-apps.fusiontable': '',
+# 'application/vnd.google-apps.map': 'application/pdf',
+# 'application/vnd.google-apps.photo': 'image/jpeg',
+# 'application/vnd.google-apps.file': '',
+# 'application/vnd.google-apps.sites': '',
+# 'application/vnd.google-apps.unknown': '',
+# 'application/vnd.google-apps.video': '',
+# 'application/vnd.google-apps.audio': '',
+# 'application/vnd.google-apps.drive-sdk': ''
+# 'application/octet-stream': 'text/plain'
 
 
 def get_credentials():
@@ -64,23 +78,19 @@ def get_credentials():
     if not os.path.exists(credential_dir):
         os.makedirs(credential_dir)
     credential_path = os.path.join(credential_dir,
-                                   'drive-python-quickstart.json')
+                                   'drive-python-sync.json')
 
     store = Storage(credential_path)
     credentials = store.get()
     if not credentials or credentials.invalid:
         flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
         flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else:  # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
+        credentials = tools.run_flow(flow, store, flags=None)
+        print('Storing credentials to ', credential_path)
     return credentials
 
 
-def get_tree(folder_name, folder_id, tree_list, 
-             root, parents_id, service):
+def get_tree(folder_name, tree_list, root, parents_id, service):
     """Gets folder tree relative paths.
 
     Recursively gets through subfolders, remembers their names ad ID's.
@@ -100,7 +110,7 @@ def get_tree(folder_name, folder_id, tree_list,
         List of folder tree relative folder paths.
 
     """
-    parents_id[folder_name] = folder_id
+    folder_id = parents_id[folder_name]
 
     results = service.files().list(
         pageSize=100,
@@ -109,7 +119,7 @@ def get_tree(folder_name, folder_id, tree_list,
         trashed != True" % folder_id)).execute()
 
     items = results.get('files', [])
-    root += folder_name + '/'
+    root += folder_name + os.path.sep
 
     for item in items:
         parents_id[item['name']] = item['id']
@@ -117,11 +127,11 @@ def get_tree(folder_name, folder_id, tree_list,
         folder_id = [i['id'] for i in items
                      if i['name'] == item['name']][0]
         folder_name = item['name']
-        get_tree(folder_name, folder_id, tree_list,
+        get_tree(folder_name, tree_list,
                  root, parents_id, service)
 
 
-def download_file_from_gdrive(path, file, service):
+def download_file_from_gdrive(file_path, drive_file, service):
     """Downloads file from Google Drive.
 
     If file is Google Doc's type, then it will be downloaded
@@ -133,33 +143,40 @@ def download_file_from_gdrive(path, file, service):
         and mimeType.
         service: Google Drive service instance.
     """
-    print(file['name'])
-    file_id = file['id']
-    if file['mimeType'] in GOOGLE_MIME_TYPES.keys():
-        request = service.files().export_media(
+    file_id = drive_file['id']
+    file_name = drive_file['name']
+    if drive_file['mimeType'] in GOOGLE_MIME_TYPES.keys():
+        if file_name.endswith(GOOGLE_MIME_TYPES[drive_file['mimeType']][1]):
+            file_name = drive_file['name']
+        else:
+            file_name = '{}{}'.format(
+                drive_file['name'],
+                GOOGLE_MIME_TYPES[drive_file['mimeType']][1])
+            service.files().update(fileId=file_id,
+                                   body={'name': file_name}).execute()
+
+        request = service.files().export(
             fileId=file_id,
-            mimeType=GOOGLE_MIME_TYPES[file['mimeType']])
-            # mimeType='application/pdf')
-        # fh = io.FileIO(os.path.join(path, file['name']), 'wb')
-        fh = io.FileIO(os.path.join(path, file['name']), 'wb')
-        downloader = MediaIoBaseDownload(fh, request)
-        done = False
-        while done is False:
-            status, done = downloader.next_chunk()
-            print("Download %d%%." % int(status.progress() * 100))
+            mimeType=(GOOGLE_MIME_TYPES[drive_file['mimeType']])[0]).execute()
+        with io.FileIO(os.path.join(file_path, file_name), 'wb') as file_write:
+            file_write.write(request)
 
     else:
         request = service.files().get_media(fileId=file_id)
-        fh = io.FileIO(os.path.join(path, file['name']), 'wb')
-        downloader = MediaIoBaseDownload(fh, request)
+        file_io = io.FileIO(os.path.join(file_path, drive_file['name']), 'wb')
+        downloader = MediaIoBaseDownload(file_io, request)
         done = False
         while done is False:
-            status, done = downloader.next_chunk()
-            # print("Download %d%%." % int(status.progress() * 100))
+            _, done = downloader.next_chunk()
 
 
-def byLines(inputStr):
-    return inputStr.count('/')
+def by_lines(input_str):
+    """Helps Sort items by the number of slashes in it.
+
+    Returns:
+        Number of slashes in string.
+    """
+    return input_str.count(os.path.sep)
 
 
 def main():
@@ -168,30 +185,25 @@ def main():
     Creates a Google Drive API service object and outputs the names and IDs
     for up to 10 files.
     """
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
+    # credentials = get_credentials()
+    http = get_credentials().authorize(httplib2.Http())
     service = discovery.build('drive', 'v3', http=http)
 
     # Get id of Google Drive folder and it's path (from other script)
     folder_id, full_path = initial_upload.check_upload(service)
-    folder_name = full_path.split('/')[-1]
-    tree_list = []
-    root = ''
-    parents_id = {}
+    folder_name = full_path.split(os.path.sep)[-1]
+    tree_list, root, parents_id = [], '', {}
 
-    About_drive = service.about().get(
-        fields='importFormats, exportFormats').execute()
-    print(About_drive)
-
-    get_tree(folder_name, folder_id, tree_list,
-             root, parents_id, service)
+    parents_id[folder_name] = folder_id
+    get_tree(folder_name, tree_list, root, parents_id, service)
     os_tree_list = []
-    root_len = len(full_path.split('/')[0:-2])
+    root_len = len(full_path.split(os.path.sep)[0:-2])
 
     # Get list of folders three paths on computer
     for root, dirs, files in os.walk(full_path, topdown=True):
         for name in dirs:
-            var_path = '/'.join(root.split('/')[root_len + 1:])
+            var_path = (os.path.sep).join(
+                root.split(os.path.sep)[root_len + 1:])
             os_tree_list.append(os.path.join(var_path, name))
 
     # old folders on computer
@@ -203,13 +215,14 @@ def main():
 
     exact_folders.append(folder_name)
 
+    var = (os.path.sep).join(full_path.split(os.path.sep)[0:-1]) + os.path.sep
+
     # Download folders from Drive
-    download_folders = sorted(download_folders, key=byLines)
+    download_folders = sorted(download_folders, key=by_lines)
 
     for folder_dir in download_folders:
-        var = '/'.join(full_path.split('/')[0:-1]) + '/'
         variable = var + folder_dir
-        last_dir = folder_dir.split('/')[-1]
+        last_dir = folder_dir.split(os.path.sep)[-1]
 
         folder_id = parents_id[last_dir]
         results = service.files().list(
@@ -220,25 +233,24 @@ def main():
         files = [f for f in items
                  if f['mimeType'] != 'application/vnd.google-apps.folder']
 
-        for file in files:
-            # file_id = f['id']
-            download_file_from_gdrive(variable, file, service)
+        for drive_file in files:
+            download_file_from_gdrive(variable, drive_file, service)
 
     # Check and refresh files in existing folders
     for folder_dir in exact_folders:
-        var = '/'.join(full_path.split('/')[0:-1]) + '/'
+        # var = '/'.join(full_path.split('/')[0:-1]) + '/'
         variable = var + folder_dir
-        last_dir = folder_dir.split('/')[-1]
+        last_dir = folder_dir.split(os.path.sep)[-1]
         os_files = [f for f in os.listdir(variable)
                     if os.path.isfile(os.path.join(variable, f))]
         folder_id = parents_id[last_dir]
 
         results = service.files().list(
-            pageSize=100,
+            pageSize=1000,
             q=('%r in parents and \
             mimeType!="application/vnd.google-apps.folder"' % folder_id),
-            fields="nextPageToken, \
-            files(id, name, mimeType, modifiedTime)").execute()
+            fields="files(id, name, mimeType, \
+                modifiedTime, md5Checksum)").execute()
 
         items = results.get('files', [])
 
@@ -247,34 +259,39 @@ def main():
         remove_files = [f for f in os_files
                         if f not in [j['name']for j in items]]
 
-        for file in refresh_files:
-            file_dir = os.path.join(variable, file['name'])
+        for drive_file in refresh_files:
+            file_dir = os.path.join(variable, drive_file['name'])
             file_time = os.path.getmtime(file_dir)
-            mtime = file['modifiedTime']
-            mtime = datetime.datetime.strptime(mtime[:-2],
+            mtime = datetime.datetime.strptime(drive_file['modifiedTime'][:-2],
                                                "%Y-%m-%dT%H:%M:%S.%f")
             drive_time = time.mktime(mtime.timetuple())
 
-            if (file_time < drive_time):
-                os.remove(os.path.join(variable, file['name']))
-                download_file_from_gdrive(variable, file, service)
-
+            file_dir = os.path.join(variable, drive_file['name'])
+            os_file_md5 = hashlib.md5(open(file_dir, 'rb').read()).hexdigest()
+            if 'md5Checksum' in drive_file.keys():
+                # print(1, file['md5Checksum'])
+                drive_md5 = drive_file['md5Checksum']
+                # print(2, os_file_md5)
             else:
-                pass
+                drive_md5 = None
 
-        for file in remove_files:
-            os.remove(os.path.join(variable, file))
+            if file_time < drive_time or (drive_md5 != os_file_md5):
+                os.remove(os.path.join(variable, drive_file['name']))
+                download_file_from_gdrive(variable, drive_file, service)
 
-        for file in upload_files:
-            download_file_from_gdrive(variable, file, service)
+        for os_file in remove_files:
+            os.remove(os.path.join(variable, os_file))
+
+        for drive_file in upload_files:
+            download_file_from_gdrive(variable, drive_file, service)
 
     # Delete old and unwanted folders from computer
-    remove_folders = sorted(remove_folders, key=byLines, reverse=True)
+    remove_folders = sorted(remove_folders, key=by_lines, reverse=True)
 
     for folder_dir in remove_folders:
-        var = '/'.join(full_path.split('/')[0:-1]) + '/'
+        # var = '/'.join(full_path.split('/')[0:-1]) + '/'
         variable = var + folder_dir
-        last_dir = folder_dir.split('/')[-1]
+        last_dir = folder_dir.split(os.path.sep)[-1]
         shutil.rmtree(variable)
 
 if __name__ == '__main__':
